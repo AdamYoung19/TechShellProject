03846652ad8c4dec7e06df21057903ceba2e9105
AdamYoung19 <ajy006@latech.edu>
2026-02-19 17:03:07 -0600

    Final

--- ShellProject.c
+++ ShellProject.c
@@ -1,5 +1,9 @@
-// Name(s): Adam Young, Jaylin Rashaw Ealy, Ethan Flanagan
-// Description: Skeleton code for a simple shell implementation in C.
+// Name(s): Ethan Flanagan, Adam Young, Jayline Ealy
+// Description:
+// A simple Unix-like shell called techshell.
+// This shell repeatedly prompts the user for input, parses commands,
+// supports built-in cd and exit, handles I/O redirection (< and >),
+// and executes commands using fork() and execvp().
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,153 +11,265 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include <errno.h>
 
-#define MAX_INPUT 1024 
+#define MAX_INPUT  255
+#define MAX_ARGS 128
+#define PATH_MAX 4096
 
-struct ShellCommand {
-    char* command;
-    char* args[100];           
+
+
+struct ShellCommand 
+{
+    char *command;
+    char *args[MAX_ARGS];
     int argCount;
-    char* inputRedirect;
-    char* outputRedirect;
-    int appendOutput;
+    char *inputFile;
+    char *outputFile;
 };
 
-// --- Function Prototypes ---
-void displayPrompt();
-char* getInput();
-struct ShellCommand parseInput(char* input);
-void executeCommand(struct ShellCommand cmd);
 
-// -- Main Function ---
-int main() {
+//////// Some function ideas: ////////////
+// Note: Some code is reflected in main that represents these functions,
+// but it is up to you to determine how you want to organize your code.
+
+/* 
+    A function that causes the prompt to 
+    display in the terminal
+*/
+void displayPrompt() 
+{
+    char cwd[PATH_MAX];
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) 
+    {
+        printf("%s$ ", cwd);
+    } else 
+    {
+        perror("getcwd");
+    }
+
+    fflush(stdout);
+}
+
+
+/*
+    A function that takes input from the user.
+    It may return return the input to the calling statement or 
+    store it at some memory location using a pointer.
+*/ 
+char* getInput() 
+{
+    char *buffer = malloc(MAX_INPUT);
+    if (!buffer) 
+    {
+        perror("malloc");
+        exit(1);
+    }
+
+    if (fgets(buffer, MAX_INPUT, stdin) == NULL) 
+    {
+        free(buffer);
+        exit(0);  // Exit on Ctrl+D
+    }
+
+    buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline
+    return buffer;
+}
+
+
+/*
+    A function that parses through the user input.
+    Consider having this function return a struct that stores vital
+    information about the parsed instruction such as:
+    - The command itself
+    - The arguments that come after the command 
+        Hint: When formatting your data, 
+        look into execvp and how it takes in args.
+    - Information about if a redirect was detected such as >, <, or |
+    - Information about whether or not a new file 
+        needs to be created and what that filename may be.
+    
+
+    Some helpful functions when doing this come from string.h and stdlib.h, such as
+    strtok, strcmp, strcpy, calloc, malloc, realloc, free, and more
+
+    Be sure to consider/test for situations when a backslash is used to escape the space char
+    and when quotes are used to group together various tokens.
+*/
+struct ShellCommand parseInput(char *input) 
+{
+
+    struct ShellCommand cmd;
+    cmd.argCount = 0;
+    cmd.inputFile = NULL;
+    cmd.outputFile = NULL;
+
+    char *token = strtok(input, " ");
+
+    while (token != NULL) 
+    {
+
+        if (strcmp(token, "<") == 0) 
+        {
+            token = strtok(NULL, " ");
+            cmd.inputFile = token;
+        }
+        else if (strcmp(token, ">") == 0) 
+        {
+            token = strtok(NULL, " ");
+            cmd.outputFile = token;
+        }
+        else 
+        {
+            cmd.args[cmd.argCount++] = token;
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    cmd.args[cmd.argCount] = NULL;
+
+    if (cmd.argCount > 0)
+        cmd.command = cmd.args[0];
+    else
+        cmd.command = NULL;
+
+    return cmd;
+}
+
+
+/*
+    A function that executes the command. 
+    This function might take in a struct that represents the shell command.
+
+    Be sure to consider each of the following:
+    1. The execvp() function. 
+        This can execute commands that already exist, that is, 
+        you don't need to recreate the functionality of 
+        the commands on your computer, just the shell.
+        Keep in mind that execvp takes over the current process.
+    2. The fork() function. 
+        This can create a process for execvp to take over.
+    3. cd is not a command like ls and mkdir. 
+        cd is a tool provided by the shell, 
+        so you WILL need to recreate the functionality of cd.
+    4. Be sure to handle standard output redirect and standard input redirects here 
+        That is, there symbols: > and <. 
+        Pipe isn't required but could be a nice addition.
+*/
+void executeCommand(struct ShellCommand cmd)
+{
+
+    if (cmd.command == NULL)
+    {
+        return;
+    }
+        
+
+    // Built-in exit
+    if (strcmp(cmd.command, "exit") == 0) 
+    {
+        exit(0);
+    }
+
+    // Built-in cd
+    if (strcmp(cmd.command, "cd") == 0)
+    {
+
+        if (cmd.argCount < 2)
+        {
+            char *home = getenv("HOME");
+            if (chdir(home) != 0)
+            {
+                printf("Error %d (%s)\n", errno, strerror(errno));
+            }
+        } 
+        else 
+        {
+            if (chdir(cmd.args[1]) != 0) 
+            {
+                printf("Error %d (%s)\n", errno, strerror(errno));
+            }
+        }
+        return;
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) 
+    {
+        perror("fork");
+        return;
+    }
+
+    if (pid == 0) 
+    {
+        // Child process
+
+        // Handle input redirection
+        if (cmd.inputFile != NULL) 
+        {
+            int fd = open(cmd.inputFile, O_RDONLY);
+            if (fd < 0) 
+            {
+                printf("Error %d (%s)\n", errno, strerror(errno));
+                exit(1);
+            }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+        }
+
+        // Handle output redirection
+        if (cmd.outputFile != NULL) 
+        {
+            int fd = open(cmd.outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0) 
+            {
+                printf("Error %d (%s)\n", errno, strerror(errno));
+                exit(1);
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+        }
+
+        execvp(cmd.command, cmd.args);
+
+        // If exec fails
+        printf("Error %d (%s)\n", errno, strerror(errno));
+        exit(1);
+    }
+    else 
+    {
+        wait(NULL); // Parent waits for child
+    }
+}
+
+
+
+int main() // MAIN
+{
     char* input;
     struct ShellCommand command;
         
     // repeatedly prompt the user for input
-    for (;;) {
+    for (;;)
+    {
         // display the prompt
         displayPrompt();
 
         // get the user's input
         input = getInput();
         
-        // Safety check: if input is NULL (e.g. user hits Ctrl+D), exit
-        if (input == NULL) break;
-
         // parse the command line
         command = parseInput(input);
         
         // execute the command
         executeCommand(command);
-        
-        // Note: You should technically free(input) here to avoid memory leaks!
-        free(input); 
+
+        free(input);
     }
 
     exit(0);
-}
-
-// --- Function Definitions ---
-
-void displayPrompt() {
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s$ ", cwd);
-    } else {
-        perror("getcwd error");
-    }
-}
-
-char* getInput() {
-    // Allocate a buffer to hold the user's input
-    char* buffer = malloc(MAX_INPUT);
-    if (buffer == NULL) return NULL;
-
-    // Read a line of input from the user
-    if (fgets(buffer, MAX_INPUT, stdin) == NULL) {
-        free(buffer);
-        return NULL;
-    }
-
-    // Strip the newline
-    buffer[strcspn(buffer, "\n")] = '\0';
-    return buffer;
-}
-
-struct ShellCommand parseInput(char* input) {
-    struct ShellCommand cmd;
-    memset(&cmd, 0, sizeof(struct ShellCommand));
-
-    int i = 0;
-    char* token = strtok(input, " ");
-    while (token != NULL) {
-        cmd.args[i] = token;
-        i++;
-        token = strtok(NULL, " ");
-    }
-    cmd.args[i] = NULL; 
-    cmd.argCount = i;
-
-    if (i > 0) {
-        cmd.command = cmd.args[0]; 
-    }
-    return cmd;
-}
-
-void executeCommand(struct ShellCommand cmd) {
-    
-    // Check if a command was actually entered
-    if (cmd.command == NULL) return;
-
-    // Handle built-in commands like 'exit'
-    if(strcmp(cmd.command, "exit") == 0) {
-        exit(0);
-    }
-
-    // Handle built-in commands like 'cd'
-    if(strcmp(cmd.command, "cd") == 0) {
-        // if argument provided
-        if (cmd.argCount > 1) {
-            if (chdir(cmd.args[1]) != 0) {
-                perror("You can't do that lol");
-            }
-            // if no argument provided, change to home directory
-            
-        }
-        else {
-                chdir(getenv("HOME"));
-            }
-        return;
-    }
-
-    // External Commands
-    pid_t pid = fork();
-
-    // In case of error
-    if (pid < 0) {
-        perror("YOU AERE A FAILURE");
-        exit(1);
-    }
-
-    // Child process
-    else if (pid == 0) {
-        // execvp looks for another command
-        execvp(cmd.command, cmd.args);
-
-
-        // Just in case execvp fails
-        printf("Error Command '%s' not found\n", cmd.command);
-        exit(1);
-    }
-
-    // Parent process
-    else {
-        int status;
-
-        //waits for the child process to finish
-        waitpid(pid, &status, 0);
-    }
-
 }
